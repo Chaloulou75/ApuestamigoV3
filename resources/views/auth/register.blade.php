660c07b5770e4ac3ba1c86d5b18b0962c038101c
Chaloulou75 <c.jeandey@gmail.com>
2020-02-18 10:44:47 -0300

    msg register errors

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,7 +15,7 @@
              @error('name') bg-red-dark @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
             @error('name')
-                <span class="hidden mt-1 text-sm text-red" role="relative px-3 py-3 mb-4 border rounded">
+                <span class=" mt-1 text-sm text-white" role="relative px-3 py-3 mb-4 border rounded">
                     <strong>{{ $message }}</strong>
                 </span>
             @enderror
@@ -28,7 +28,7 @@
                 <input id="email" type="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('email') bg-red-dark @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="email" autofocus>
 
                 @error('email')
-                    <span class="hidden mt-1 text-sm text-red" role="relative px-3 py-3 mb-4 border rounded">
+                    <span class=" mt-1 text-sm text-white" role="relative px-3 py-3 mb-4 border rounded">
                         <strong>{{ $message }}</strong>
                     </span>
                 @enderror           
@@ -85,7 +85,7 @@
               </select>
 
                 @if ($errors->has('club'))
-                    <span class="hidden mt-1 text-sm text-red" role="relative px-3 py-3 mb-4 border rounded">
+                    <span class="mt-1 text-sm text-white" role="relative px-3 py-3 mb-4 border rounded">
                         <strong>{{ $errors->first('club') }}</strong>
                     </span>
                 @endif            
@@ -98,7 +98,7 @@
             <input id="password" type="password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('password') bg-red-dark @enderror" name="password" required autocomplete="new-password">
 
             @error('password')
-                <span class="hidden mt-1 text-sm text-red" role="relative px-3 py-3 mb-4 border rounded">
+                <span class="mt-1 text-sm text-white" role="relative px-3 py-3 mb-4 border rounded">
                     <strong>{{ $message }}</strong>
                 </span>
             @enderror            
