b057041b3f0fac47913f96cac11914a195661c96
Chaloulou75 <c.jeandey@gmail.com>
2020-02-18 11:43:05 -0300

    slected team after errors

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,49 +39,49 @@
                 {{ __('all.Favorite Club') }}
             </label>            
               <select id="club" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-900 leading-tight focus:outline-none focus:shadow-outline {{ $errors->has('club') ? ' bg-red-dark' : '' }}" name="club" value="{{ old('club') }}" required>              
-                <option value="Borussia Dortmund">Borussia Dortmund</option>
-                <option value="Bayer Leverkusen">Bayer Leverkusen</option>
-                <option value="FC Bayern">FC Bayern</option>
-                <option value="FC Schalke">FC Schalke</option>
-                <option value="RB Leipzig">RB Leipzig</option>                
-                <option value="TSG Hoffenheim">TSG Hoffenheim</option>
-                <option value="Atletico Madrid">Atletico Madrid</option>
-                <option value="FC Barcelona">FC Barcelona</option>
-                <option value="Real Madrid">Real Madrid</option>
-                <option value="Valence CF">Valence CF</option>                
-                <option value="CSKA Moscow">CSKA Moscow</option>
-                <option value="Zénith Saint-Petersbourg">Zénith Saint-Pétersbourg</option>                
-                <option value="Lokomotiv Moscow">Lokomotiv Moscow</option>
-                <option value="RB Salzburg">RB Salzburg</option>                 
-                <option value="Galatasaray">Galatasaray</option>
-                <option value="Juventus FC">Juventus FC</option>
-                <option value="FC Inter">FC Inter</option>
-                <option value="Atalanta Bergame">Atalanta Bergame</option>               
-                <option value="SSC Napoli">SSC Napoli</option>
-                <option value="AS Roma">AS Roma</option>
-                <option value="Liverpool FC" selected="selected">Liverpool FC</option>
-                <option value="Chelsea FC">Chelsea FC</option>
-                <option value="Manchester City">Manchester City</option>
-                <option value="Manchester United">Manchester United</option>
-                <option value="Tottenham Hotspur">Tottenham Hotspur</option>
-                <option value="Slavia Prague">Slavia Prague</option>                
-                <option value="Olympique Lyonnais">Olympique Lyonnais</option>
-                <option value="Paris SG">Paris SG</option>
-                <option value="LOSC Lille">LOSC Lille</option>
-                <option value="AS Monaco">AS Monaco</option>
-                <option value="FC Porto">FC Porto</option>
-                <option value="Benfica">Benfica</option>
-                <option value="PSV Eindhoven">PSV Eindhoven</option>
-                <option value="Ajax Amsterdam">Ajax Amsterdam</option>                
-                <option value="Red Star Belgrade">Red Star Belgrade</option>
-                <option value="Dinamo Zagreb">Dinamo Zagreb</option> 
-                <option value="Shakhtar Donetsk">Shakhtar Donetsk</option>
-                <option value="Viktoria Plzeň">Viktoria Plzeň</option>
-                <option value="Brugge FC">Brugge FC</option>
-                <option value="Racing Genk">Racing Genk</option>
-                <option value="Olympiakos">Olympiakos</option>
-                <option value="Young Boys Berne">Young Boys Berne</option>
-                <option value="AEK Athen">AEK Athen</option>  
+                <option value="Borussia Dortmund"  @if(old('club') == 'Borussia Dortmund')selected @endif>Borussia Dortmund</option>
+                <option value="Bayer Leverkusen" @if(old('club') == 'Bayer Leverkusen')selected @endif>Bayer Leverkusen</option>
+                <option value="FC Bayern" @if(old('club') == 'FC Bayern')selected @endif>FC Bayern</option>
+                <option value="FC Schalke" @if(old('club') == 'FC Schalke')selected @endif>FC Schalke</option>
+                <option value="RB Leipzig" @if(old('club') == 'RB Leipzig')selected @endif>RB Leipzig</option>                
+                <option value="TSG Hoffenheim" @if(old('club') == 'TSG Hoffenheim')selected @endif>TSG Hoffenheim</option>
+                <option value="Atletico Madrid" @if(old('club') == 'Atletico Madrid')selected @endif>Atletico Madrid</option>
+                <option value="FC Barcelona" @if(old('club') == 'FC Barcelona')selected @endif>FC Barcelona</option>
+                <option value="Real Madrid" @if(old('club') == 'Real Madrid')selected @endif>Real Madrid</option>
+                <option value="Valence CF" @if(old('club') == 'Valence CF')selected @endif>Valence CF</option>                
+                <option value="CSKA Moscow" @if(old('club') == 'CSKA Moscow')selected @endif>CSKA Moscow</option>
+                <option value="Zénith Saint-Petersbourg" @if(old('club') == 'Zénith Saint-Petersbourg')selected @endif>Zénith Saint-Pétersbourg</option>                
+                <option value="Lokomotiv Moscow" @if(old('club') == 'Lokomotiv Moscow')selected @endif>Lokomotiv Moscow</option>
+                <option value="RB Salzburg" @if(old('club') == 'RB Salzburg')selected @endif>RB Salzburg</option>                 
+                <option value="Galatasaray" @if(old('club') == 'Galatasaray')selected @endif>Galatasaray</option>
+                <option value="Juventus FC" @if(old('club') == 'Juventus FC')selected @endif>Juventus FC</option>
+                <option value="FC Inter" @if(old('club') == 'FC Inter')selected @endif>FC Inter</option>
+                <option value="Atalanta Bergame" @if(old('club') == 'Atalanta Bergame')selected @endif>Atalanta Bergame</option>               
+                <option value="SSC Napoli" @if(old('club') == 'SSC Napoli')selected @endif>SSC Napoli</option>
+                <option value="AS Roma" @if(old('club') == 'AS Roma')selected @endif>AS Roma</option>
+                <option value="Liverpool FC" @if(old('club') == 'Liverpool FC')selected @endif>Liverpool FC</option>
+                <option value="Chelsea FC" @if(old('club') == 'Chelsea FC')selected @endif>Chelsea FC</option>
+                <option value="Manchester City" @if(old('club') == 'Manchester City')selected @endif>Manchester City</option>
+                <option value="Manchester United" @if(old('club') == 'Manchester United')selected @endif>Manchester United</option>
+                <option value="Tottenham Hotspur" @if(old('club') == 'Tottenham Hotspur')selected @endif>Tottenham Hotspur</option>
+                <option value="Slavia Prague" @if(old('club') == 'Slavia Prague')selected @endif>Slavia Prague</option>                
+                <option value="Olympique Lyonnais" @if(old('club') == 'Olympique Lyonnais')selected @endif>Olympique Lyonnais</option>
+                <option value="Paris SG" @if(old('club') == 'Paris SG')selected @endif>Paris SG</option>
+                <option value="LOSC Lille" @if(old('club') == 'LOSC Lille')selected @endif>LOSC Lille</option>
+                <option value="AS Monaco" @if(old('club') == 'AS Monaco')selected @endif>AS Monaco</option>
+                <option value="FC Porto" @if(old('club') == 'FC Porto')selected @endif>FC Porto</option>
+                <option value="Benfica" @if(old('club') == 'Benfica')selected @endif>Benfica</option>
+                <option value="PSV Eindhoven" @if(old('club') == 'PSV Eindhoven')selected @endif>PSV Eindhoven</option>
+                <option value="Ajax Amsterdam" @if(old('club') == 'Ajax Amsterdam')selected @endif>Ajax Amsterdam</option>                
+                <option value="Red Star Belgrade" @if(old('club') == 'Red Star Belgrade')selected @endif>Red Star Belgrade</option>
+                <option value="Dinamo Zagreb" @if(old('club') == 'Dinamo Zagreb')selected @endif>Dinamo Zagreb</option> 
+                <option value="Shakhtar Donetsk" @if(old('club') == 'Shakhtar Donetsk')selected @endif>Shakhtar Donetsk</option>
+                <option value="Viktoria Plzeň" @if(old('club') == 'Viktoria Plzeň')selected @endif>Viktoria Plzeň</option>
+                <option value="Brugge FC" @if(old('club') == 'Brugge FC')selected @endif>Brugge FC</option>
+                <option value="Racing Genk" @if(old('club') == 'Racing Genk')selected @endif>Racing Genk</option>
+                <option value="Olympiakos" @if(old('club') == 'Olympiakos')selected @endif>Olympiakos</option>
+                <option value="Young Boys Berne" @if(old('club') == 'Young Boys Berne')selected @endif>Young Boys Berne</option>
+                <option value="AEK Athen" @if(old('club') == 'AEK Athen')selected @endif>AEK Athen</option>  
               </select>
 
                 @if ($errors->has('club'))
