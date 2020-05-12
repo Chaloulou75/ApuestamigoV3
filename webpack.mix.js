a6c2b24fb6515169e190201c22bf000384349ad5
Chaloulou75 <c.jeandey@gmail.com>
2020-05-12 15:31:50 -0300

    back

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,6 @@
 let mix = require('laravel-mix');
 require('laravel-mix-tailwind');
 
-require('laravel-mix-purgecss');
-
 const WebpackShellPlugin = require('webpack-shell-plugin');
 const path = require('path')
 
@@ -36,5 +34,4 @@ mix.js('resources/js/app.js', 'public/js')
 	            ziggy: path.resolve('vendor/tightenco/ziggy/dist/js/route.js'),
 	        },
 	    },
-	})
-   	.purgeCss();
+	});
