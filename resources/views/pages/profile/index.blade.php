51ec035ea37f563a50530aa6165e4631df4c3c7b
Chaloulou75 <c.jeandey@gmail.com>
2020-06-20 17:44:42 -0300

    few changes2

--- resources/views/pages/profile/index.blade.php
+++ resources/views/pages/profile/index.blade.php
@@ -17,7 +17,7 @@
 		<div class="text-white text-sm">			
 			<ul class="list-disc">
 				@foreach($users as $user)
-					<li class="animate__animated animate__lightSpeedInLeft py-4 px-2">id <span class="text-blue-500">{{$user->id}}</span>: <span class="text-francaverde">{{$user->name}}</span> - <span class="text-gray-500">({{$user->email }} / admin: {{$user->admin}})</span> dont l'équipe préférée est <span class="text-gray-500">{{$user->equipe->name}} </span> <img class="inline w-10 h-10" loading="lazy" src="{{ $user->equipe->logourl ? url($user->equipe->logourl) : URL::to('/img/' .$user->equipe->logo) }}"></span>  
+					<li class="animate__animated animate__lightSpeedInLeft py-4 px-2">id <span class="text-blue-500">{{$user->id}}</span>: <span class="text-francaverde">{{$user->name}}</span> - <span class="text-gray-500">({{$user->email }} / admin: {{ $user->admin }} / créé le: {{ \Carbon\Carbon::parse($user->created_at)->isoFormat('dddd Do MMMM YYYY H:mm') }})</span> dont l'équipe préférée est <span class="text-gray-500">{{$user->equipe->name}} </span> <img class="inline w-10 h-10" loading="lazy" src="{{ $user->equipe->logourl ? url($user->equipe->logourl) : URL::to('/img/' .$user->equipe->logo) }}"></span>  
 						<a class="px-2 text-francaverde" href="{{route('profile.show', $user)}}"> Show {{$user->name}} </a>
 						<form class="inline-block" method="POST" action="{{route('profile.destroy', $user)}}"> 
 					  		@csrf 
