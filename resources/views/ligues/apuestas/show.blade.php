e256976901db23aec282bd2e273681b7b4758aa7
Chaloulou75 <c.jeandey@gmail.com>
2020-07-03 20:59:32 -0300

    refonte des journee12

--- resources/views/ligues/apuestas/show.blade.php
+++ resources/views/ligues/apuestas/show.blade.php
@@ -75,8 +75,6 @@
 		  	&& isset($resultAdmin->matchs[$key]->game_id) 
 		  	&& $game->id == $resultAdmin->matchs[$key]->game_id)
 		  	{{ $resultAdmin->matchs[$key]->resultatEq1 }}
-			@else
-			{{ $game->id }} . {{ $resultAdmin->matchs[$key]->id }}
 		  	@endif
 		  	<span class="text-gray-900 px-1">-</span>  
 		  	@if(isset($resultAdmin->matchs[$key]->resultatEq2)
