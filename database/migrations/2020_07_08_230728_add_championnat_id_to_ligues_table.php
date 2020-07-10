21e6c3726f7534c853ff542964a3d43165a63ed4
Chaloulou75 <c.jeandey@gmail.com>
2020-07-10 17:51:46 -0300

    enlever on up on del cascade champid4

---
+++ database/migrations/2020_07_08_230728_add_championnat_id_to_ligues_table.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddChampionnatIdToLiguesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ligues', function (Blueprint $table) {
+            $table->unsignedBigInteger('championnat_id')->nullable()->after('id');
+            $table->foreign('championnat_id')->references('id')->on('championnats');
+        });
+
+        Schema::table('matches', function (Blueprint $table) {
+            $table->unsignedBigInteger('championnat_id')->nullable()->after('id');
+            $table->foreign('championnat_id')->references('id')->on('championnats');
+        });
+
+        Schema::table('date_journees', function (Blueprint $table) {
+            $table->unsignedBigInteger('championnat_id')->nullable()->after('id');
+            $table->foreign('championnat_id')->references('id')->on('championnats');
+        });
+
+        Schema::table('games', function (Blueprint $table) {
+            $table->unsignedBigInteger('championnat_id')->nullable()->after('id');
+            $table->foreign('championnat_id')->references('id')->on('championnats');
+        });
+
+        Schema::table('equipes', function (Blueprint $table) {
+            $table->unsignedBigInteger('championnat_id')->nullable()->after('id');
+            $table->foreign('championnat_id')->references('id')->on('championnats');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('ligues', function (Blueprint $table) {
+            $table->dropColumn('championnat_id');
+        });
+
+        Schema::table('matches', function (Blueprint $table) {
+            $table->dropColumn('championnat_id');
+        });
+
+        Schema::table('date_journees', function (Blueprint $table) {
+            $table->dropColumn('championnat_id');
+        });
+
+        Schema::table('games', function (Blueprint $table) {
+            $table->dropColumn('championnat_id');
+        });
+
+        Schema::table('equipes', function (Blueprint $table) {
+            $table->dropColumn('championnat_id');
+        });
+    }
+}
