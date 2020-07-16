6eb76f3a6c7e28d8f64746912a06234cc4076b08
Chaloulou75 <c.jeandey@gmail.com>
2020-07-16 14:07:25 -0300

    package csp 1st try

---
+++ app/Services/Csp/Policies/Extended.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Services\Csp\Policies;
+
+use Spatie\Csp\Directive;
+use Spatie\Csp\Policies\Policy;
+
+class Extended extends Policy
+{
+    public function configure()
+    {
+        parent::configure();
+        
+        $this
+        	->addDirectivesForGoogleFonts()
+            ->addDirectivesForGoogleAnalytics()
+            ->addDirectivesForGoogleTagManager()
+            ->addDirectivesForStripe()
+            ->addDirective(Directive::IMG, ['*'])
+            ->addNonceForDirective(Directive::SCRIPT)
+        	->addNonceForDirective(Directive::STYLE)
+    }
+
+    protected function addDirectivesForGoogleFonts(): self
+    {
+        return $this
+            ->addDirective(Directive::FONT, 'fonts.gstatic.com')
+            ->addDirective(Directive::SCRIPT, 'fonts.googleapis.com')
+            ->addDirective(Directive::STYLE, 'fonts.googleapis.com');
+    }
+
+    protected function addDirectivesForGoogleAnalytics(): self
+    {
+        return $this->addDirective(Directive::SCRIPT, '*.google-analytics.com');
+    }
+
+    protected function addDirectivesForStripe(): self
+    {
+        return $this->addDirective(Directive::SCRIPT, '*.js.stripe.com');
+    }
+
+    protected function addDirectivesForGoogleTagManager(): self
+    {
+        return $this->addDirective(Directive::SCRIPT, '*.googletagmanager.com');
+    }
+}
