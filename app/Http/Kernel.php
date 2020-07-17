1e013fa89642d6b724839105069697de145ea852
Chaloulou75 <c.jeandey@gmail.com>
2020-07-17 14:45:47 -0300

    csp basic sans kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\FrameHeadersMiddleware::class,
-            \Spatie\Csp\AddCspHeaders::class,
+            //\Spatie\Csp\AddCspHeaders::class,
 
         ],
 
